7c6afbe8e458d8b9ddd2d90e3970e10e42973b40
nsriv531 <nsriv531@mtroyal.ca>
2024-10-16 14:24:51 -0600

    hope this works

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>F1 Dashboard Project</title>
+    <link rel="stylesheet" href="index.css">
+    <!-- Import Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&family=Lobster&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <h1>F1 Dashboard Project</h1>
+        <div class="nav-links">
+            <a href="index.php">Home</a>
+            <a href="browse.php">Browse</a>
+            <a href="apitester.php">APIs</a>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="description">
+            <p>Provide a brief description, styled nicely, about what this site is about, what technologies you are using, the group member names, and the URL for the GitHub repo.</p>
+            <a href="browse.php" class="button">Browse 2022 Season</a>
+            <a href="browse.php" class="link">Link to browse page</a>
+        </div>
+        <div class="image-section">
+            <p>Some type of relevant photo or image: could be a race car, a race circuit, a squirrel, whatever, it's up to you.</p>
+        </div>
+    </div>
+</body>
+</html>
